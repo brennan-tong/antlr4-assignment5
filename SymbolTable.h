06fb732a8b2a9bb9ed84b4cd72858a3fd49b040d
brennan-tong <96040135+brennan-tong@users.noreply.github.com>
2023-04-21 21:08:55 -0700

    Update SymbolTable.h

--- SymbolTable.h
+++ SymbolTable.h
@@ -1,38 +1,36 @@
-#ifndef SYMBOL_TABLE_H
-#define SYMBOL_TABLE_H
+#pragma once
 
-#include <any>
-#include <antlr4-runtime.h>
-#include <unordered_map>
-#include <string>
 #include <stack>
+#include <map>
+#include <string>
+#include <vector>
 #include "TypedefStructure.h"
+#include <antlr4-runtime.h>
 #include <ostream>
 
-enum class IdentifierKind { VARIABLE, CONSTANT, TYPEDEF, FUNCTION };
+enum class IdentifierKind {VARIABLE, CONSTANT, TYPEDEF, FUNCTION, PROGRAM};
 
 struct SymbolTableEntry {
     std::string identifier;
     IdentifierKind kind;
-    antlrcpp::Any value;
+    std::string type;
 };
 
 class SymbolTable {
 public:
     SymbolTable();
-
     void pushScope();
     void popScope();
-
     void addConstant(const std::string &identifier, const antlrcpp::Any &value);
     void addVariable(const std::string &identifier, const std::string &type);
-    void addTypedef(const std::string &identifier, const Typedef &typeDef);
+    void addTypedef(const std::string &identifier, const std::string &typeDef);
     void addFunction(const std::string &identifier, const std::string &returnType);
-
     bool isDeclaredInCurrentScope(const std::string &identifier) const;
+    void addProgram(const std::string &identifier);
     void print(std::ostream &os = std::cout) const;
-private:
-    std::stack<std::unordered_map<std::string, SymbolTableEntry>> scopes;
-};
 
-#endif // SYMBOL_TABLE_H
+private:
+    std::stack<std::map<std::string, SymbolTableEntry>> scopes;
+    std::stack<std::vector<std::string>> orderedIdentifiers;
+    std::string programName;
+};
